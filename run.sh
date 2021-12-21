0b060eafdabfaa48b3e9a7f3d097fe210fa73765
Shohei Fujii <fujii.shohei@gmail.com>
2021-12-21 20:35:10 +0800

    minor update

--- run.sh
+++ run.sh
@@ -3,11 +3,10 @@ source .env
 
 xhost +local: 
 if [ ! -f $XAUTH ] ; then
+	touch $XAUTH
     xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
     if [ ! -z "$xauth_list" ] ; then
         echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
     fi
     chmod a+r $XAUTH
 fi
